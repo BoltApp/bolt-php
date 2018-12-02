4871a6e784169566cad74a34eb02cdde0c12c47f
Daniel Dragic <danac@guaranteed.site>
2018-12-02 21:15:36 +0100

    create composer package, set version to 1.0.0, add notice of licencse to every php file

--- example/order_confirmation.php
+++ example/order_confirmation.php
@@ -1,3 +1,18 @@
 <?php
+/**
+ * Bolt PHP library
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License (MIT)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category   Bolt
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    https://opensource.org/licenses/MIT  MIT License (MIT)
+ */
+
 /** Dummy success page*/
 echo "Your order was successfully placed!";
